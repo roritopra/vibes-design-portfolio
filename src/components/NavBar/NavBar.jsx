c55c48f2cb18de2cface9ea73ab6e36630a0901e
roritopra <1193102598@u.icesi.edu.co>
2023-10-22 02:13:27 -0500

    Checkpoint

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,27 +1,29 @@
+import { NavBarBrand } from "./NavBarBrand";
+import { navLinks } from "../navLinks/navLinks";
+
 function NavBar() {
   return (
     <>
       <nav className="flex items-center">
-        <div className="flex items-center justify-center w-20% bg-[#383838] border-2 border-solid border-[#383838] py-[10px] cursor-pointer">
-          <img className="w-[62px]" src="/icons/logo.svg" alt="" />
+        <NavBarBrand />
+        <div className="hidden items-center justify-center bg-white border-2 border-solid border-[#383838] py-[10px] cursor-pointer maxLg:w-[100%] maxLg:flex ">
+          <img className="w-[42px] h-[50px]" src="/icons/menu_mobile.svg" alt="" />
         </div>
-        <div>
-
-        </div>
-        <ul className="flex items-center w-full">
-          <li className="flex items-center justify-center w-20% border-[#383838] border-2 py-[21px] bg-white font-darkestMedium text-lg cursor-pointer">
+        
+        <ul className="flex items-center w-full maxLg:hidden">
+          <li className="flex items-center justify-center w-20% text-[#383838] border-[#383838] border-2 py-[21px] bg-white font-darkestMedium text-lg cursor-pointer">
               Home
           </li>
-          <li className="flex items-center justify-center w-20% border-[#383838] border-2 py-[21px] bg-white font-darkestMedium text-lg cursor-pointer">
+          <li className="flex items-center justify-center w-20% text-[#383838] border-[#383838] border-2 py-[21px] bg-white font-darkestMedium text-lg cursor-pointer">
               Projects 
           </li>
-          <li className="flex items-center justify-center w-20% border-[#383838] border-2 py-[21px] bg-white font-darkestMedium text-lg cursor-pointer">
+          <li className="flex items-center justify-center w-20% text-[#383838] border-[#383838] border-2 py-[21px] bg-white font-darkestMedium text-lg cursor-pointer">
               Contact
           </li>
-          <li className="flex items-center justify-center w-20% border-[#383838] border-2 py-[21px] bg-white font-darkestMedium text-lg cursor-pointer">
+          <li className="flex items-center justify-center w-20% text-[#383838] border-[#383838] border-2 py-[21px] bg-white font-darkestMedium text-lg cursor-pointer">
               About Us
           </li>
-          <li className="flex items-center justify-center w-20% border-[#383838] border-2 py-[21px] bg-[#A8AFF0] font-darkestMedium text-lg cursor-pointer">
+          <li className="flex items-center justify-center w-20% text-white border-[#383838] border-2 py-[21px] bg-[#A8AFF0] font-darkestMedium text-lg cursor-pointer">
               Log in
           </li>
         </ul>
