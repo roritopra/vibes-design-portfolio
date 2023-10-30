db83353e960361cfc451a7dec20c1efa6186ef51
andresrojassshere <andres.camilo.rojas@outlook.com>
2023-10-30 15:14:05 -0500

    Correción Fotos

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -13,13 +13,13 @@ export function AboutUsPage() {
           <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif" />
         </div>
         <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#B4FFC9] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
-          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif"/>
+          <img className="w-full" src="/imgs/andres_photo.png" alt="Gif"/>
         </div>
         <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#EE7E3F] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
-          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif" />
+          <img className="w-full" src="/imgs/mariana_photo.png" alt="Gif" />
         </div>
         <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#D6EE5E] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
-          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif" />
+          <img className="w-full" src="/imgs/alejo_photo.png" alt="Gif" />
         </div>
       </div>
       <div className="pl-0 w-full flex flex-col md:flex-row justify-start items-start bg-white pt-4">
@@ -168,7 +168,7 @@ export function AboutUsPage() {
             UNIVERSIDAD ICESI - DISEÑO DE MEDIOS INTERACTIVOS
           </p>
           <p className="p-1 text-md text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
-            (2019-2023) Currently in 8th semester.
+            (2020-2023) Currently in 6th semester.
           </p>
         </div>
         <div className="pl-0 md:pl-8 w-full md:w-40% flex flex-col">
@@ -192,6 +192,69 @@ export function AboutUsPage() {
           </li>
           <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
           My weakness is freshly brewed coffee, which I often savor while working on my personal projects. 
+          </li>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div className="bg-[#F3F3F3] min-h-screen flex justify-center items-center border-t-2 border-solid border-[#383838]">
+    <div className="mx-auto my-4 bg-white p-4 border border-dashed border-black w-11/12 max-w-6xl">
+      <div className="flex flex-col md:flex-row ">
+        <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#A8AFF0] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
+          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif" />
+        </div>
+        <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#B4FFC9] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
+          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif"/>
+        </div>
+        <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#EE7E3F] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
+          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif" />
+        </div>
+        <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#D6EE5E] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
+          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif" />
+        </div>
+      </div>
+      <div className="pl-0 w-full flex flex-col md:flex-row justify-start items-start bg-white pt-4">
+        <h2 className="p-1 text-[#EE7E3F] text-5xl font-bagnard maxMd:text-3xl maxMd:p-4">
+          Alejandro Ruedas
+        </h2>
+      </div>
+      <div className="pl-0 w-full flex flex-col md:flex-row justify-start items-start bg-white p-2">
+        <div className="w-full md:w-60%">
+          <p className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          I am Alejandro, a passionate guy about various types of artistic expressions, and one of my favorites is design. I enjoy creating logos with meaning, detailed illustrations, and I especially love animated design. In addition, I have a strong preference for clean, orderly, and minimalist aesthetics.
+          </p>
+          <p className="p-1 text-3xl text-[#EE7E3F] font-darkestBold border-b-2 border-solid border-[#EE7E3F] maxMd:text-base maxMd:p-4">
+            Studies
+          </p>
+          <p className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+            UNIVERSIDAD ICESI - DISEÑO DE MEDIOS INTERACTIVOS
+          </p>
+          <p className="p-1 text-md text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+            (2021-2023) Currently in 6th semester.
+          </p>
+        </div>
+        <div className="pl-0 md:pl-8 w-full md:w-40% flex flex-col">
+          <p className="p-1 text-3xl text-[#EE7E3F] font-darkestBold border-b-2 border-solid border-[#EE7E3F] maxMd:text-base maxMd:p-4">
+            Experience
+          </p>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          Logos and promotional materials for small businesses
+          </li>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          Web Design.
+          </li>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          Animations and cartoons, as well as producing illustrations.
+          </li>
+          <p className="p-1 text-3xl text-[#EE7E3F] font-darkestBold border-b-2 border-solid border-[#EE7E3F] maxMd:text-base maxMd:p-4">
+            Hobbies
+          </p>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          I enjoy doing excersice like crossfit and swimming.
+          </li>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          My weakness is dancing and music. 
           </li>
         </div>
       </div>
