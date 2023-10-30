8d05761cb2919103d9b6918155e08448bb3e3dea
AlejandroRv19 <98325607+AlejandroRv19@users.noreply.github.com>
2023-10-29 23:34:52 -0500

    Login and register

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,17 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
-import { Header } from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
+import { Routes, Route } from "react-router-dom"
 //Aquí se importan los componentes que se van a usar en las rutas
 
 export function AppRouter() {
   return (
     <main>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-      <Footer />
+        <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
     </main>
-  );
+  )
 }
+
