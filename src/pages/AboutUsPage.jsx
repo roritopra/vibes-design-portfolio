0e61bb26c912bfac13991f9e04b342c1de28d791
andresrojassshere <andres.camilo.rojas@outlook.com>
2023-10-30 03:10:27 -0500

    Info Mariana AboutUs

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -134,6 +134,74 @@ export function AboutUsPage() {
       </div>
     </div>
   </div>
+
+  <div className="bg-[#F3F3F3] min-h-screen flex justify-center items-center border-t-2 border-solid border-[#383838]">
+    <div className="mx-auto my-4 bg-white p-4 border border-dashed border-black w-11/12 max-w-6xl">
+      <div className="flex flex-col md:flex-row ">
+        <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#A8AFF0] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
+          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif" />
+        </div>
+        <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#B4FFC9] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
+          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif"/>
+        </div>
+        <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#EE7E3F] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
+          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif" />
+        </div>
+        <div className="w-full md:w-1/2 h-1/4 md:w-1/4 bg-[#D6EE5E] border-solid border-[#383838] border-t-2 md:border-b-2 md:border-l-2 md:border-r-2">
+          <img className="w-full" src="/imgs/rodro_photo.png" alt="Gif" />
+        </div>
+      </div>
+      <div className="pl-0 w-full flex flex-col md:flex-row justify-start items-start bg-white pt-4">
+        <h2 className="p-1 text-[#EE7E3F] text-5xl font-bagnard maxMd:text-3xl maxMd:p-4">
+          Mariana Ocampo
+        </h2>
+      </div>
+      <div className="pl-0 w-full flex flex-col md:flex-row justify-start items-start bg-white p-2">
+        <div className="w-full md:w-60%">
+          <p className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          My leadership, charisma, exceptional efficiency, and a touch of creative impatience guarantee brilliant results delivered in record time. My outgoing energy and passionate commitment to design and programming make me an invaluable asset for Vibes Design.
+          </p>
+          <p className="p-1 text-3xl text-[#EE7E3F] font-darkestBold border-b-2 border-solid border-[#EE7E3F] maxMd:text-base maxMd:p-4">
+            Studies
+          </p>
+          <p className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+            UNIVERSIDAD ICESI - DISEÑO DE MEDIOS INTERACTIVOS
+          </p>
+          <p className="p-1 text-md text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+            (2019-2023) Currently in 8th semester.
+          </p>
+        </div>
+        <div className="pl-0 md:pl-8 w-full md:w-40% flex flex-col">
+          <p className="p-1 text-3xl text-[#EE7E3F] font-darkestBold border-b-2 border-solid border-[#EE7E3F] maxMd:text-base maxMd:p-4">
+            Experience
+          </p>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+            OOH Experiences
+          </li>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          Designed children's books, magazine covers, and developed website.
+          </li>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          Currently bringing a clothing brand to life from scratch, taking care of everything from the logo to clothing prints.
+          </li>
+          <p className="p-1 text-3xl text-[#EE7E3F] font-darkestBold border-b-2 border-solid border-[#EE7E3F] maxMd:text-base maxMd:p-4">
+            Hobbies
+          </p>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          I enjoy exploring new places, whether it's walking down unfamiliar streets or visiting museums and cinemas.
+          </li>
+          <li className="p-1 text-lg text-[#373737] font-darkestMedium maxMd:text-base maxMd:p-4">
+          My weakness is freshly brewed coffee, which I often savor while working on my personal projects. 
+          </li>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
 </main>
 
 
