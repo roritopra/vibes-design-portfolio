e5406fd9fd5c4a551454e4a2173ecb0731f56ab5
roritopra <1193102598@u.icesi.edu.co>
2023-11-14 22:27:18 -0500

    [UPDATE] Added isActive navbar responsive

--- src/user/components/NavBar/NavBar.jsx
+++ src/user/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import { menuLinks } from "./menuLinks/menuLinks";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
 export function NavBar() {
@@ -16,37 +15,57 @@ export function NavBar() {
 
   return (
     <>
-    <div className={`fixed top-0 left-0 z-50 w-screen h-screen bg-[#F3F3F3] transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-      <button className="absolute top-4 right-4 text-[#383838] text-5xl" onClick={toggleNavbar}>
-        &#9776;
-      </button>
-      <div className="flex flex-col items-center justify-center h-full">
-        <ul className="flex flex-col gap-10">
-          {menuLinks.map((menu, index) => (
-            <Link
-              to={menu.path}
-              key={index}
-              onClick={closeNavbar}
-              className={`flexr font-darkestBold text-3xl text-center cursor-pointer transition-all ${
-                index === menuLinks.length - 1
-                  ? "text-[#ffffff] !bg-[#A8AFF0] pb-3 pt-2 px-20 hover:!bg-[#848ac2]"
-                  : ""
-              }`}
+      <div
+        className={`fixed top-0 left-0 z-50 w-screen h-screen bg-[#F3F3F3] transform transition-transform duration-300 ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <button
+          className="absolute top-4 right-4 text-[#383838] text-5xl"
+          onClick={toggleNavbar}
+        >
+          &#x2716;
+        </button>
+        <div className="flex flex-col items-center justify-center h-full">
+          <ul className="flex flex-col gap-10">
+            {menuLinks.map((menu, index) => (
+              <NavLink
+                to={menu.path}
+                key={index}
+                onClick={closeNavbar}
+                className={({ isActive }) => {
+                  return `font-darkestBold text-3xl text-center cursor-pointer transition-all ${
+                    isActive ? "bg-[#bfbfbf] py-3 px-6 text-black" : ""
+                  }`;
+                }}
+              >
+                {menu.text}
+              </NavLink>
+            ))}
+            <NavLink
+              to="/login"
+              className={({ isActive }) => {
+                return `font-darkestBold text-3xl bg-[#A8AFF0] py-3 px-6 text-white" text-center cursor-pointer transition-all ${
+                  isActive ? "bg-[#848ac2] py-3 px-6 text-white" : ""
+                }`;
+              }}
             >
-              {menu.text}
-            </Link>
-          ))}
-        </ul>
+              Login
+            </NavLink>
+          </ul>
+        </div>
       </div>
-    </div>
 
-      <div className="hidden items-center justify-center bg-white border-2 border-solid border-[#383838] py-[10px] cursor-pointer maxLg:w-[100%] maxLg:flex">
-        <button onClick={toggleNavbar}>
-        <img
-          className="w-[30px] h-[50px]"
-          src="/icons/menu_mobile.svg"
-          alt=""
-        />
+      <div
+        onClick={toggleNavbar}
+        className="hidden items-center justify-center bg-white border-2 border-solid border-[#383838] py-[10px] cursor-pointer maxLg:w-[100%] maxLg:flex"
+      >
+        <button>
+          <img
+            className="w-[30px] h-[50px]"
+            src="/icons/menu_mobile.svg"
+            alt=""
+          />
         </button>
       </div>
 
@@ -60,22 +79,21 @@ export function NavBar() {
                 isActive ? "bg-[#bfbfbf] py-3 px-6 text-black" : ""
               }`;
             }}
-
-
           >
             {menu.text}
-          </NavLink>         
-        ))}
-        <NavLink to="/login"
-        className={({ isActive }) => {
-          return `flex items-center justify-center w-20% text-white border-[#383838] border-2 py-[21px] bg-[#A8AFF0] font-darkestBold text-lg cursor-pointer transition-all hover:bg-[#848ac2] hover:transition-all ${
-            isActive ? "bg-[#848ac2] py-3 px-6 text-white" : ""
-          }`;
-        }}>
-            Login
           </NavLink>
+        ))}
+        <NavLink
+          to="/login"
+          className={({ isActive }) => {
+            return `flex items-center justify-center w-20% text-white border-[#383838] border-2 py-[21px] bg-[#A8AFF0] font-darkestBold text-lg cursor-pointer transition-all hover:bg-[#848ac2] hover:transition-all ${
+              isActive ? "bg-[#848ac2] py-3 px-6 text-white" : ""
+            }`;
+          }}
+        >
+          Login
+        </NavLink>
       </ul>
     </>
   );
 }
-
